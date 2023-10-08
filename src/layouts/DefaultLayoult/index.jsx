79438c09d5c37559bbd576e937b2a7bbf7d4ca7a
caioNunesS <caiogrb@gmail.com>
2023-10-08 12:24:59 -0300

    fix: testes da home

--- src/layouts/DefaultLayoult/index.jsx
+++ src/layouts/DefaultLayoult/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from "../../Components/Navbar";
+import {Navbar} from "../../Components/Navbar";
 import Footer from '../../Components/Footer';
 
 export const DefaultLayout = () => {
